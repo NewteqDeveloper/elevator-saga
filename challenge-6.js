6816518d2f2178e00e48c556a205770231ffbd6e
Sanity1123 <Sanity1123@users.noreply.github.com>
2015-11-22 18:28:00 +0200

    Update challenge-6.js
    Trying to get the toggling to work.

--- challenge-6.js
+++ challenge-6.js
@@ -1,11 +1,36 @@
 {
     init: function(elevators, floors) {
         $.each(elevators, function(index, elevator){
-            elevator.goingUpIndicator(true);
-            elevator.goingDownIndicator(false);
+            var goingUp = function(){
+                elevator.goingUpIndicator(true);
+                elevator.goingDownIndicator(false);
+            };
+            var goingDown = function(){
+                elevator.goingUpIndicator(false);
+                elevator.goingDownIndicator(true);  
+            };
+            var toggleIndicator = function(){
+                var up = elevator.goingUpIndicator();
+                var down = elevator.goingDownIndicator();
+                
+                elevator.goingUpIndicator(!up);
+                elevator.goingDownIndicator(!down);
+            };
+            
+            goingDown();
+            
+            elevator.on("idle", function(){
+                toggleIndicator();
+            });
             
             elevator.on("floor_button_pressed", function(floor){
                 elevator.goToFloor(floor);
+                if (floor === 3){
+                    goingDown();
+                }
+                if (floor === 0){
+                    goingUp();
+                }
             });
             
             $.each(floors, function(index, floor){
