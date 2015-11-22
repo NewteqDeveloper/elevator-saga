52538ab51c745a34a3ba47c96c27c42fc2b4f225
Sanity1123 <Sanity1123@users.noreply.github.com>
2015-11-22 12:59:50 +0200

    Update challenge-2.js
    This achieves 18 people in 60 seconds

--- challenge-2.js
+++ challenge-2.js
@@ -2,6 +2,7 @@
     init: function(elevators, floors) {
         // Do stuff with the elevators and floors, which are both arrays of objects
         var elevator = elevators[0];
+        
         elevator.on("idle", function() {
             elevator.goToFloor(0);
             elevator.goToFloor(1);
@@ -9,10 +10,15 @@
             elevator.goToFloor(3);
             elevator.goToFloor(4);
         });
+        
         elevator.on("stopped_at_floor", function(floor){
             if (floor === 4){
                 elevator.goToFloor(0);
             }
+        });
+        
+        elevator.on("floor_button_pressed", function(floor){
+            elevator.goToFloor(floor, true);
         });
     },
     update: function(dt, elevators, floors) {
