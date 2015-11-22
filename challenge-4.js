61b00b54d3218a29aa8b5c9c3e1240fa1bf53148
Sanity1123 <Sanity1123@users.noreply.github.com>
2015-11-22 17:30:40 +0200

    Update challenge-4.js
    Caters for all elevators but challenge still fails. Only able to transport 26 people in 60 seconds

--- challenge-4.js
+++ challenge-4.js
@@ -1,27 +1,26 @@
 {
     init: function(elevators, floors) {
-        // Do stuff with the elevators and floors, which are both arrays of objects
-        var elevator = elevators[0];
-        
-        if (elevator.loadFactor() >= 0.5){
-            var pressed = elevator.getPressedFloors();
-            $.each(pressed, function(index, floor){
-               elevator.goToFloor(floor, true); 
+        $.each(elevators, function(index, elevator){
+            if (elevator.loadFactor() >= 0.5){
+                var pressed = elevator.getPressedFloors();
+                $.each(pressed, function(index, floor){
+                    elevator.goToFloor(floor, true); 
+                });
+            }
+
+            $.each(floors, function(index, floor){
+                floor.on("up_button_pressed", function(){
+                    elevator.goToFloor(floor.floorNum());
+                });
+
+                floor.on("down_button_pressed", function(){
+                    elevator.goToFloor(floor.floorNum());
+                });
             });
-        }
-        
-        $.each(floors, function(index, floor){
-           floor.on("up_button_pressed", function(){
-               elevator.goToFloor(floor.floorNum());
-           });
-            
-           floor.on("down_button_pressed", function(){
-               elevator.goToFloor(floor.floorNum());
-           });
-        });
-        
-        elevator.on("floor_button_pressed", function(floor){
-            elevator.goToFloor(floor, true);
+
+            elevator.on("floor_button_pressed", function(floor){
+                elevator.goToFloor(floor, true);
+            }); 
         });
     },
     update: function(dt, elevators, floors) {
