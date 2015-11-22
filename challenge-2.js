4a96d62124a8f691ca988929fc4a3b62a505592f
Sanity1123 <Sanity1123@users.noreply.github.com>
2015-11-22 16:30:41 +0200

    Challenege 2 - 20 people in 59 seconds

--- challenge-2.js
+++ challenge-2.js
@@ -3,14 +3,6 @@
         // Do stuff with the elevators and floors, which are both arrays of objects
         var elevator = elevators[0];
         
-        elevator.on("idle", function() {
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2);
-            elevator.goToFloor(3);
-            elevator.goToFloor(4);
-        });
-        
         elevator.on("stopped_at_floor", function(floor){
             if (floor === 4){
                 elevator.goToFloor(0);
