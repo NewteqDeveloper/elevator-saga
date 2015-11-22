c739d46c0fbd3ba1b8297bc1a52a0efc9ab8a9ef
Sanity1123 <Sanity1123@users.noreply.github.com>
2015-11-22 17:33:16 +0200

    Update challenge-4.js
    Transported 29 people in 60 seconds

--- challenge-4.js
+++ challenge-4.js
@@ -1,12 +1,6 @@
 {
     init: function(elevators, floors) {
         $.each(elevators, function(index, elevator){
-            if (elevator.loadFactor() >= 0.5){
-                var pressed = elevator.getPressedFloors();
-                $.each(pressed, function(index, floor){
-                    elevator.goToFloor(floor, true); 
-                });
-            }
 
             $.each(floors, function(index, floor){
                 floor.on("up_button_pressed", function(){
@@ -24,7 +18,14 @@
         });
     },
     update: function(dt, elevators, floors) {
-        // Do more stuff with the elevators and floors
         // dt is the number of game seconds that passed since the last time update was called
+        $.each(elevators, function(index, elevator){
+            if (elevator.loadFactor() >= 0.5){
+                var pressed = elevator.getPressedFloors();
+                $.each(pressed, function(index, floor){
+                    elevator.goToFloor(floor, true); 
+                });
+            }  
+        })
     }
 }
