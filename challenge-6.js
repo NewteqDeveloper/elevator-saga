c61956c3e4b9d88711aa16297ee9f581a3da8f28
Sanity1123 <Sanity1123@users.noreply.github.com>
2015-11-22 18:15:15 +0200

    Update challenge-6.js

--- challenge-6.js
+++ challenge-6.js
@@ -1,10 +1,23 @@
 {
     init: function(elevators, floors) {
         $.each(elevators, function(index, elevator){
+            elevator.goingUpIndicator(true);
+            elevator.goingDownIndicator(false);
+            
+            elevator.on("floor_button_pressed", function(floor){
+                elevator.goToFloor(floor);
+            });
+            
+            $.each(floors, function(index, floor){
+                
+            });
             
         });
     },
     update: function(dt, elevators, floors) {
         // dt is the number of game seconds that passed since the last time update was called
+        $.each(elevators, function(index, elevator){
+           
+        });
     }
 }
